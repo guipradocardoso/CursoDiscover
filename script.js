1229eb55b19a4af6b0372b38418d8ea532e9a4fd
guipradocardoso <guilhermeprado-09@hotmail.com>
2023-07-31 23:33:37 -0300

    Atualizando HTML com JavaScript

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 function toggleMode() {
   const html = document.documentElement
-
-  // if(html.classList.contains('light')){
-  //   html.classList.remove('light')
-  // } else{
-  //   html.classList.add('light')
-  // }
-
   html.classList.toggle("light")
+
+  // pegar a tag img
+  const img = document.querySelector("#profile img")
+
+  // substituir a imagem
+  if (html.classList.contains("light")) {
+    // se tver light mode, adicionar a imagem light
+    img.setAttribute("src", "./assets/avatar-light.png")
+    img.setAttribute("alt", " Esta Imagem está no modo Light")
+  } else {
+    // se tiver sem light mode, manter a imagem normal
+    img.setAttribute("src", "./assets/avatar.png")
+    img.setAttribute("alt", "Está Imagem está no Modo Dark")
+  }
 }
